bcb6cd561d34af42b84656154411541c862336b4
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-03-30 16:34:17 +0700

    first commit

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+// let users = [
+//     {userName: 'admin', password: 'admin'}
+// ];
+// // truy cap vao cac o input
+// const $userLogin=document.getElementById('user-login');
+// console.log($userLogin);
+// const $formLogin=document.getElementById('container-form');
+// console.log($formLogin);
+
+// $userLogin.onclick=function(){
+//     $formLogin.style.display="display:block;";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
