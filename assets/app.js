d490c6be64054d61830ee5c85ea1f219d81e6cae
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-04-01 17:58:38 +0700

    “new”

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,35 @@
-// let users = [
-//     {userName: 'admin', password: 'admin'}
-// ];
-// // truy cap vao cac o input
-// const $userLogin=document.getElementById('user-login');
-// console.log($userLogin);
-// const $formLogin=document.getElementById('container-form');
-// console.log($formLogin);
+const btnCloseModalElm = document.getElementById('btnCloseModal');
+const formLoginElm = document.getElementById('formLogin');
+console.log(formLoginElm)
+const modalElm = document.getElementById('modal');
+const containerFormElm = document.getElementById('containerForm');
 
-// $userLogin.onclick=function(){
-//     $formLogin.style.display="display:block;";
-// }
+console.log("containerFormElm: ", containerFormElm)
+
+btnCloseModalElm.addEventListener('click', function() {
+    modalElm.style.display = 'none';
+});
+formLoginElm.addEventListener('click', function() {
+    modalElm.style.display = 'block';
+});
+modalElm.addEventListener('click', function() {
+    modalElm.style.display = 'none';
+});
+containerFormElm.addEventListener('click', function(event) {
+    console.log('title: ', event);
+    event.stopPropagation();
+});
 
 
+const btnDangKy=document.getElementById('btnDangKy');
+console.log(btnDangKy);
 
+const modalDangKy=document.getElementById('modal-dangky');
+console.log(modalDangKy)
+
+modalDangKy.addEventListener('onclick',function(){
+btnDangKy.style.display='block';
+});
 
 
 
