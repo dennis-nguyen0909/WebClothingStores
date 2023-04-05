88ba0e6d3d2001959c35b62c7195b055f304b386
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-04-05 21:26:22 +0700

    new

--- assets/app.js
+++ assets/app.js
@@ -46,6 +46,15 @@ modalDangKy.addEventListener('click', function() {
     modalDangKy.style.display = 'none';
 });
 
+// SLIDER========================================
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 3000);
 
 
 
